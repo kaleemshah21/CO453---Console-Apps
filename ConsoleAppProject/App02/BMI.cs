c9e28e8fceaa4e1184e1b63d4ccbd7e7346073d9
kaleemshah21 <80137233+kaleemshah21@users.noreply.github.com>
2024-01-22 19:10:14 +0000

    Added Web App for App02

--- ConsoleAppProject/App02/BMI.cs
+++ ConsoleAppProject/App02/BMI.cs
@@ -17,13 +17,15 @@ namespace ConsoleAppProject.App02
     /// </author>
     public class BMI
     {
-        public double weight, height, bmi;
-        public double kilograms;
-        public int stones;
-        public double pounds;
-        public int feet;
-        public double inches;
-        public double metres;
+        public double weight, height;
+        public double Bmi { get; set; }
+        public double Kilograms { get; set; }
+        public int Stones { get; set; }
+        public double Pounds { get; set; }
+        public int Feet { get; set; }
+        public double Inches { get; set; }
+        public double Metres { get; set; }
+
         public string choice;
         public string BMImessage;
 
@@ -46,8 +48,8 @@ namespace ConsoleAppProject.App02
                 GetImperialBMI();
                 BMImessage = GetBMIMessage();
                 Console.WriteLine(BMImessage);
-                DisplayBAMEMessage();
-
+                string bameMessage = DisplayBAMEMessage();
+                Console.WriteLine(bameMessage);
                 //Console.Write(Math.Round(bmi,2));
             }
             if (choice == "2")
@@ -57,31 +59,33 @@ namespace ConsoleAppProject.App02
                 GetMetricBMI();
                 BMImessage = GetBMIMessage();
                 Console.WriteLine(BMImessage);
-                DisplayBAMEMessage();
+                string bameMessage = DisplayBAMEMessage();
+                Console.WriteLine(bameMessage);
                 //Console.Write(Math.Round(bmi, 2));
             }
 
         }
 
-        private void GetImperialBMI()
+        public void GetImperialBMI()
         {
             //BMI = (weight in pounds) x 703 / (height in inches)2
-            pounds = (stones * 14) + pounds;
-            inches = (feet * 12) + inches;
-            bmi = (((pounds) * 703)) / ((inches) * (inches));
+            Pounds = (Stones * 14) + Pounds;
+            Inches = (Feet * 12) + Inches;
+            Bmi = (((Pounds) * 703)) / ((Inches) * (Inches));
         }
 
-        private void GetMetricBMI()
+        public void GetMetricBMI()
         {
             //BMI = (weight in kg) / (height in metres)2
-            bmi = (kilograms / (metres * metres));
+            Bmi = (Kilograms / (Metres * Metres));
         }
 
-        public void DisplayBAMEMessage()
+        public string DisplayBAMEMessage()
         {
-            Console.WriteLine("\n if you are Black, Asian or other minority ethnic groups, " +
+            string bameMessage = ("\n if you are Black, Asian or other minority ethnic groups, " +
                 " you have a higher risk, Adults at 23.0 or more are at increased risk," +
                 " Adults at 27.5 or more are at high risk.");
+            return bameMessage;
         }
         private static string DisplayMenu(string prompt)
         {
@@ -97,20 +101,24 @@ namespace ConsoleAppProject.App02
 
         private void GetImperialWeight()
         {
+            int StoneInput;
+            double PoundsInput;
             
             Console.Write(" Enter weight in stones: ");
-            while (!int.TryParse(Console.ReadLine(), out stones) || stones < 0)
+            while (!int.TryParse(Console.ReadLine(), out StoneInput) || StoneInput < 0)
             {
                 DisplayErrorMessage(" Invalid input. Please enter a valid positive number for stones.");
                 Console.Write(" Enter weight in stones: ");
             }
+            Stones = StoneInput;
 
             Console.Write(" Enter weight in pounds: ");
-            while (!double.TryParse(Console.ReadLine(), out pounds) || pounds < 0 || pounds >= 14)
+            while (!double.TryParse(Console.ReadLine(), out PoundsInput) || PoundsInput < 0 || PoundsInput >= 14)
             {
                 DisplayErrorMessage(" Invalid input. Please enter a valid positive number less than 14 for pounds.");
                 Console.Write(" Enter weight in pounds: ");
             }
+            Pounds = PoundsInput;
 
             
         }
@@ -118,79 +126,85 @@ namespace ConsoleAppProject.App02
         public string GetBMIMessage()
         {
             StringBuilder message = new StringBuilder();
-            if (bmi < Underweight)
+            if (Bmi < Underweight)
             {
-                message.Append($" Your BMI is {bmi:0.00}," +
+                message.Append($" Your BMI is {Bmi:0.00}," +
                     $"You are underweight");
             }
-            else if (bmi < Normal)
+            else if (Bmi < Normal)
             {
-                message.Append($" Your BMI is {bmi:0.00}," +
+                message.Append($" Your BMI is {Bmi:0.00}," +
                     $"You are normal");
             }
-            else if(bmi < Overweight){
-                message.Append($" Your BMI is {bmi:0.00}," +
+            else if(Bmi < Overweight){
+                message.Append($" Your BMI is {Bmi:0.00}," +
                     $"You are overweight");
             }
-            else if (bmi < ObeseCI)
+            else if (Bmi < ObeseCI)
             {
-                message.Append($" Your BMI is {bmi:0.00}," +
+                message.Append($" Your BMI is {Bmi:0.00}," +
                     $"You are Obese Class 1");
             }
-            else if (bmi < ObeseCII)
+            else if (Bmi < ObeseCII)
             {
-                message.Append($" Your BMI is {bmi:0.00}," +
+                message.Append($" Your BMI is {Bmi:0.00}," +
                     $"You are Obese Class 2");
             }
             else
             {
-                message.Append($" Your BMI is {bmi:0.00}," +
+                message.Append($" Your BMI is {Bmi:0.00}," +
                     $"You are Obese Class 3");
             }
             return message.ToString();
         }
         private void GetImperialHeight()
         {
-            
+            double InchesInput;
+            int FeetInput;
+
             Console.Write(" Enter height in feet: ");
-            while (!int.TryParse(Console.ReadLine(), out feet) || feet < 0)
+            while (!int.TryParse(Console.ReadLine(), out FeetInput) || FeetInput < 0)
             {
                 DisplayErrorMessage(" Invalid input. Please enter a valid positive number for feet.");
                 Console.Write(" Enter height in feet: ");
             }
+            Feet = FeetInput;
 
             Console.Write(" Enter height in inches: ");
-            while (!double.TryParse(Console.ReadLine(), out inches) || inches < 0 || inches >= 12)
+            while (!double.TryParse(Console.ReadLine(), out InchesInput) || InchesInput < 0 || InchesInput >= 12)
             {
                 DisplayErrorMessage(" Invalid input. Please enter a valid positive number less than 12 for inches.");
                 Console.Write(" Enter height in inches: ");
+                
             }
+            Inches = InchesInput;
 
-            
         }
 
         private void GetMetricWeight()
         {
+            double KilogramsInput;
             
             Console.Write(" Enter weight in Kilograms: ");
-            while (!double.TryParse(Console.ReadLine(), out kilograms) || kilograms < 1)
+            while (!double.TryParse(Console.ReadLine(), out KilogramsInput) || KilogramsInput < 1)
             {
                 DisplayErrorMessage(" Invalid input. Please enter a valid positive number for Kilograms.");
                 Console.Write(" Enter weight in Kilograms: ");
             }
-            //return kilograms;
+            Kilograms = KilogramsInput;
         }
 
         private void GetMetricHeight()
         {
+            double MetresInput;
            
             Console.Write(" Enter Height in Metres: ");
-            while (!double.TryParse(Console.ReadLine(), out metres) || metres < 1)
+            while (!double.TryParse(Console.ReadLine(), out MetresInput) || MetresInput < 1)
             {
                 DisplayErrorMessage(" Invalid input. Please enter a valid positive number for Kilograms.");
                 Console.Write(" Enter Height in Metres: ");
             }
-            //return metres;
+            Metres = MetresInput;
 
         }
 
--- WebApps/Controllers/HomeController.cs
+++ WebApps/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using WebApps.Models;
 //creates a reference to the Distance converter
 using ConsoleAppProject.App01;
+using ConsoleAppProject.App02;
 
 
 namespace WebApps.Controllers
@@ -24,6 +25,34 @@ namespace WebApps.Controllers
                 converter.CalculateDistance();
             }
             return View(converter);
+        }
+        [HttpGet]
+        public IActionResult BMI()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult BMI(BMI bmiCalculator)
+        {
+            if (bmiCalculator.Metres > 1.40)
+            {
+                bmiCalculator.GetMetricBMI();
+            }
+            else if(bmiCalculator.Feet > 4 && bmiCalculator.Stones > 6)
+            {
+                bmiCalculator.GetImperialBMI();
+            }
+            else
+            {
+                ViewBag.Error = "You have entered values too small for any adult";
+                return View();
+            }
+            double bmiIndex = bmiCalculator.Bmi;
+            return RedirectToAction("HealthMessage", new { bmiIndex });
+        }
+        public IActionResult HealthMessage(double bmiIndex)
+        {
+            return View(bmiIndex);
         }
 
         public IActionResult Privacy()
